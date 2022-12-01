70172fe26f1b2e7a8dec68866bd3cac2314851eb
sekolas <kemerserkan067@gmail.com>
2022-12-01 23:44:51 +0300

    odev7

--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -1,76 +1,105 @@
 var express = require('express');
 var router = express.Router();
 
- const anaSayfa=function(req, res, next) {
-    res.render('anasayfa', { 
-      "baslik": "Anasayfa",
-      "sayfaBaslik": {
-        "siteAd": "MekanBul",
-        "slogan": "Civardaki Mekanları Keşfet!"
-      },
-      "mekanlar": [
-        {
-          "ad": "Starbucks",
-          "adres": "Centrum Garden AVM",
-          "puan": "4",
-          "imkanlar": ["Dünya Kahveleri","Kekler","Pastalar"],
-          "mesafe": "10km"
-        }
-        ,
-        {
-          "ad": "Gloria Jean's Coffees",
-          "adres": "SDÜ Doğu Kampüsü",
-          "puan": "3",
-          "imkanlar": ["Kahve","Çay","Pasta"],
-          "mesafe": "5km"
-        }
-      ]
+const axios=require("axios");
+var apiSecenekleri={
+  sunucu:"http://localhost:3000",
+  //sunucu:"https://mekanbul.mertcantrker.repl.co",
+  apiYolu:"/api/mekanlar/"
+}
+
+var mesafeyiFormatla=function(mesafe){
+var yeniMesafe,birim;
+if(mesafe>1){
+  yeniMesafe=parseFloat(mesafe).toFixed(1);
+  birim=" km";
+}else{
+  yeniMesafe=parseInt(mesafe*1000,10);
+  birim=" m";
+  
+}
+return yeniMesafe+birim;
+}
+
+var anaSayfaOlustur=function(res,mekanListesi){
+  var mesaj;
+  if(!(mekanListesi instanceof Array)){
+    mesaj="API hatası: Birşeyler ters gitti";
+    mekanListesi=[];
+  }else{
+    if(!mekanListesi.length){
+      mesaj="cicarda hiç mekan yok";
     }
-    );
   }
+  res.render("anasayfa",{
+    "baslik":"Anasayfa",
+    "sayfaBaslik":{
+      "siteAd":"Mekanbul",
+      "slogan":"Civardaki Mekanları Keşfet"
+    },
+    "mekanlar":mekanListesi,
+    "mesaj":mesaj
 
-  const mekanBilgisi=function(req, res, next) {
-    res.render('mekanbilgisi',
-      {
-        "baslik": "Mekan Bilgisi",
-        "mekanBaslik": "Starbucks",
-        "mekanDetay": {
-          "ad": "Starbucks",
-          "adres": "Centrum Garden AVM",
-          "puan": "4",
-          "imkanlar": ["Dünya Kahveleri","Kekler","Pastalar"],
-          "koordinatlar": {
-            "enlem": "37.7",
-            "boylam":"30.5"
-          },
-          "saatler":[{
-            "gunler":"Pazartesi-Cuma",
-            "acilis":"9:00",
-            "kapanis":"23:00",
-            "kapali": false
-        },
-        {
-            "gunler":"Cumartesi-Pazar",
-            "acilis":"10:00",
-            "kapanis":"22:00",
-            "kapali": false
-        }
-    ],
-          "yorumlar": [
-            {
-              "yorumYapan":"Serkan Kemer",
-              "puan":"4",
-              "tarih":"20 Ekim 2022",
-              "yorumMetni":"Kahveler çok iyiydi."
-            },
-          ]
-        }
-      });
+
+  });
+}
+const anaSayfa=function(req, res) {
+  axios.get(apiSecenekleri.sunucu+apiSecenekleri.apiYolu,{
+    params:{
+      enlem:req.query.enlem,
+      boylam:req.query.boylam
+    }
+  }).then(function(response){
+    var i,mekanlar;
+    mekanlar=response.data;
+    for(i=0;i<mekanlar.length;i++){
+      mekanlar[i].mesafe=mesafeyiFormatla(mekanlar[i].mesafe);
+    }
+    anaSayfaOlustur(res,mekanlar);
+  }).catch(function(hata){
+    anaSayfaOlustur(res,hata);
+  });
+}
+
+var detaySayfasiOlustur=function(res,mekanDetaylari){
+  mekanDetaylari.koordinat={
+      "enlem":mekanDetaylari.koordinat[0],
+      "boylam":mekanDetaylari.koordinat[1]
   }
+  res.render('mekanbilgisi',
+  {
+      mekanBaslik:mekanDetaylari.ad,
+      mekanDetay:mekanDetaylari
+  });
+}
 
-  const YorumEkle=function(req, res, next) {
+var hataGoster = function(res,hata){
+  var mesaj;
+  if(hata.response.status==404){
+      mesaj="404, Sayfa Bulunamadı";
+  }else{
+      mesaj=hata.response.status+" hatası";
+  }
+  res.status(hata.response.status);
+  res.render('error',{
+  "mesaj":mesaj
+  });
+};
+
+const mekanBilgisi=function(req,res){
+  axios
+   .get(apiSecenekleri.sunucu+apiSecenekleri.apiYolu+req.params.mekanid)
+   .then(function(response){
+       detaySayfasiOlustur(res,response.data);
+   })
+   .catch(function(hata){
+       hataGoster(res,hata);
+   });
+};
+
+const YorumEkle=function(req, res, next) {
     res.render('yorumekle', { title: 'Yorum Ekle' });
-  };
+}
 
   module.exports={
     anaSayfa,
